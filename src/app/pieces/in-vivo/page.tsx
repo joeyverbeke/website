4b975a3021177cd9b6d0266ca450e25ab09dfb54
joeyverbeke <joeyverbeke@gmail.com>
2025-06-10 18:39:50 -0400

    wonky is squanky

--- src/app/pieces/in-vivo/page.tsx
+++ src/app/pieces/in-vivo/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+//wonky is very squanky
+
 import { useEffect, useRef, useState, useCallback } from 'react';
 import HomeButton from '@/components/HomeButton';
 import { Eye, EyeClosed } from 'lucide-react';
